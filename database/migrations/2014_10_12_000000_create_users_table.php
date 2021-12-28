3da3ccdeb0c6db6dfaee55f77be3b3ddda4942eb
ayatmustafa <ayatmustafa199472@gmail.com>
2021-12-28 18:06:24 +0200

    in progress setup project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('user_name')->unique();
+            $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('user_image')->nullable();
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedTinyInteger('receive_email')->default(0);
+            $table->text('bio')->nullable();
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
